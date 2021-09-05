69a10f572dbed56ff9db670719bd3ce5f8a453c7
wilhsie <wjhsie@gmail.com>
2021-09-05 14:26:05 -0700

    testing continuous deploy

--- src/App.js
+++ src/App.js
@@ -34,15 +34,7 @@ class LambdaDemo extends Component {
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <LambdaDemo />
-        </header>
-      </div>
+      <h1>ohaiyou gozaimasu</h1>
     )
   }
 }
